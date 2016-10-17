39730653bc085a20f1b44b0e0b693cfa418789e6
Kargin Nikita <hei@spark-mail.ru>
2016-10-17 21:55:26 +0300

    Created ArrayCollection implementation of Collection i-face

---
+++ src/ArrayCollection.java
@@ -0,0 +1,103 @@
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class ArrayCollection<T> implements Collection<T> {
+
+
+    private int size = 0;
+
+    @SuppressWarnings("unchecked")
+    private T[] m = (T[]) new Object();
+
+    @Override
+    public int size() {
+        return this.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        for (Object value : this.m) {
+            if (value == o) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ElementsIterator();
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Object[] toArray() {
+        final T[] newM = (T[]) new Object[this.size()];
+        System.arraycopy(m, 0, newM, 0, this.size());
+        return newM;
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(T t) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    private class ElementsIterator implements Iterator<T> {
+
+        private int size;
+
+        @Override
+        public boolean hasNext() {
+            return ArrayCollection.this.size() > size;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return ArrayCollection.this.m[size++];
+        }
+    }
+}
