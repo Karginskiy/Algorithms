c687c2956f8428d063497590d9d6169ef86eee78
Kargin Nikita <hei@spark-mail.ru>
2016-10-18 17:18:08 +0300

    Added benchmark as test to ArrayCollection

--- src/main/java/jmhtests/ArrayCollectionTest.java
+++ src/main/java/jmhtests/ArrayCollectionTest.java
@@ -1,4 +1,22 @@
 package jmhtests;
 
+import collections.ArrayCollection;
+import org.openjdk.jmh.annotations.Benchmark;
+
+import java.util.Collection;
+
 public class ArrayCollectionTest {
+
+    @Benchmark
+    public void addTest() {
+        Collection<Integer> collection = new ArrayCollection<>();
+
+        int iterations = 10000;
+
+        for(int i = 0; i < iterations; i++) {
+            collection.add(i);
+        }
+
+    }
+
 }
