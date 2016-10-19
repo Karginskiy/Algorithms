726fc014321292fca783ed2dcbcd942f2db99244
Kargin Nikita <hei@spark-mail.ru>
2016-10-19 20:46:41 +0300

    Some changes

--- src/main/java/jmhtests/ArrayCollectionTest.java
+++ src/main/java/jmhtests/ArrayCollectionTest.java
@@ -2,15 +2,19 @@ package jmhtests;
 
 import collections.ArrayCollection;
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
 
 import java.util.Collection;
 
+@State(value = Scope.Benchmark)
 public class ArrayCollectionTest {
+
 
     @Benchmark
     public void addTest() {
-        Collection<Integer> collection = new ArrayCollection<>();
 
+        Collection<Integer> collection = new ArrayCollection<>();
         int iterations = 10000;
 
         for(int i = 0; i < iterations; i++) {
