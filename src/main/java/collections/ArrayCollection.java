83a162125cb88c3bbf65c56ebf7b44857c22f966
Kargin Nikita <hei@spark-mail.ru>
2016-10-19 20:46:34 +0300

    New implementation of toArray

--- src/main/java/collections/ArrayCollection.java
+++ src/main/java/collections/ArrayCollection.java
@@ -1,177 +1,154 @@
 package collections;
 
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.NoSuchElementException;
+import java.util.Iterator;
 
 public class ArrayCollection<T> implements Collection<T> {
 
-    @SuppressWarnings("unchecked")
-    private T[] m = (T[])new Object[10];
+    private T[] m = (T[])new Object[1];
 
-    private int size = 0;
+    private int size;
 
     @Override
     public int size() {
-        // BEGIN (write your solution here)
         return this.size;
-        // END
     }
 
     @Override
     public boolean isEmpty() {
-        // BEGIN (write your solution here)
         return this.size() == 0;
-        // END
     }
 
     @Override
     public boolean contains(final Object o) {
-        // BEGIN (write your solution here)
-        for (Object value : m) {
-            if (value == o) {
-                return true;
-            }
+        for (int i = 0; i < size; i++) {
+            if (m[i].equals(o)) return true;
         }
         return false;
-        // END
     }
 
     @Override
     public Iterator<T> iterator() {
-        // BEGIN (write your solution here)
         return new ElementsIterator();
-        // END
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public Object[] toArray() {
-        // BEGIN (write your solution here)
-        final T[] newM = (T[]) new Object[this.size()];
+        final T[] newM = (T[])new Object[this.size()];
         System.arraycopy(m, 0, newM, 0, this.size());
         return newM;
-        // END
     }
 
     @Override
-    @SuppressWarnings("unchecked")
-    public <T1> T1[] toArray(T1[] a) {
+    public <T1> T1[] toArray(final T1[] a) {
         // BEGIN (write your solution here)
-        return (T1[]) this.toArray();
-        // END
+        if (a.length < size)
+            return (T1[]) Arrays.copyOf(m, size, a.getClass());
+
+        System.arraycopy(m, 0, a, 0, size);
+
+        if (a.length > size)
+            a[size] = null;
+
+        return a;
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public boolean add(final T t) {
-        // BEGIN (write your solution here)
         if (m.length == size) {
             final T[] oldM = m;
             m = (T[]) new Object[this.size() * 2];
             System.arraycopy(oldM, 0, m, 0, oldM.length);
-            m[size++] = t;
-            return true;
-        } else {
-            m[size++] = t;
         }
-        return false;
-        // END
+        m[size++] = t;
+        return true;
     }
 
     @Override
     public boolean remove(final Object o) {
-        // BEGIN (write your solution here)
-        for (int i = 0; i < this.size(); i++) {
+        for (int i = 0; i < size(); i++) {
             if (m[i].equals(o)) {
-                if (i != this.size() - 1) {
-                    System.arraycopy(m, i + 1, m, i, this.size() - i);
-                }
-                this.size--;
+                this.remove(i);
                 return true;
             }
         }
         return false;
-        // END
     }
 
     @Override
     public boolean containsAll(final Collection<?> c) {
-        // BEGIN (write your solution here)
-        for (Object value : c) {
-            if (!this.contains(value)) {
-                return false;
-            }
+        for (final Object item : c) {
+            if (!this.contains(item)) return false;
         }
         return true;
-        // END
     }
 
     @Override
     public boolean addAll(final Collection<? extends T> c) {
-        // BEGIN (write your solution here)
-        for (T obj : c) {
-            this.add(obj);
+        for (final T item : c) {
+            add(item);
         }
         return true;
-        // END
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public boolean removeAll(final Collection<?> c) {
-        // BEGIN (write your solution here)
-        for (Object obj : c) {
-            this.remove((T) obj);
-            return true;
+        for (final Object item : c) {
+            remove(item);
         }
-        return false;
-        // END
+        return true;
     }
 
     @Override
     public boolean retainAll(final Collection<?> c) {
-        // BEGIN (write your solution here)
-        for (T obj : this) {
-            if (!c.contains(obj)) this.remove(obj);
-            size--;
+        for (final Object item : this) {
+            if (!c.contains(item)) this.remove(item);
         }
         return true;
-        // END
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public void clear() {
-        // BEGIN (write your solution here)
-        this.size = 0;
-        this.m = (T[]) new Object[size];
-        // END
+        m = (T[])new Object[1];
+        size = 0;
+    }
+
+    private void remove(final int index) {
+        if (index != this.size() - 1)
+            System.arraycopy(m, index + 1, m, index, this.size() - index - 1);
+        size--;
     }
 
     private class ElementsIterator implements Iterator<T> {
-        // BEGIN (write your solution here)
-        private int size = 0;
 
+        private int index;
+
+        private int last = -1;
+
+        @Override
         public boolean hasNext() {
-            return ArrayCollection.this.size() > size;
-        }
-
-        public T next() {
-            if (!hasNext()) {
-                throw new NoSuchElementException();
-            }
-            return ArrayCollection.this.m[size++];
+            return ArrayCollection.this.size() > index;
         }
 
         @Override
-        public void remove() {
-            if (size < ArrayCollection.this.size() - 1) {
-                System.arraycopy(ArrayCollection.this.m, size + 1,
-                        ArrayCollection.this.m, size, ArrayCollection.this.size() - size - 1);
-            }
+        public T next() {
+            if (!hasNext())
+                throw new NoSuchElementException();
+            last = index;
+            return ArrayCollection.this.m[index++];
         }
-        // END
+
+        public void remove() {
+            if (last == -1)
+                throw new IllegalStateException();
+            ArrayCollection.this.remove(last);
+            index--;
+            last = -1;
+        }
+
     }
+
 
 }
