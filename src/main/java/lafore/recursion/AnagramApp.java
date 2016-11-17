9d599a5ecc62b5a28e7cd708bf6a3438a75e0171
Karginskiy <hei@spark-mail.ru>
2016-11-17 16:19:29 +0300

    Recursive anagram for String

---
+++ src/main/java/lafore/recursion/AnagramApp.java
@@ -0,0 +1,69 @@
+package lafore.recursion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AnagramApp {
+    static int size;
+    static int count;
+    static char[] arrChar = new char[100];
+
+    public static void main(String[] args) throws IOException {
+        System.out.print("Enter a word: ");
+        String input = getString();
+        size = input.length();
+        count = 0;
+        for (int i = 0; i < size; i++) {
+            arrChar[i] = input.charAt(i);
+        }
+        doAnagram(size);
+    }
+
+    public static void doAnagram(int newSize) {
+        if (newSize == 1) {
+            return;
+        }
+        for (int i = 0; i < newSize; i++) {
+            doAnagram(newSize - 1);
+            if (newSize == 2) {
+                displayWord();
+            }
+            rotate(newSize);
+        }
+    }
+
+    public static void rotate(int newSize) {
+        int j;
+        int position = size - newSize;
+        char temp = arrChar[position];
+        for (j = position + 1; j < size; j++) {
+            arrChar[j - 1] = arrChar[j];
+        }
+        arrChar[j - 1] = temp;
+    }
+
+    public static String getString() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        return bufferedReader.readLine();
+    }
+
+    public static void displayWord() {
+        if (count < 99) {
+            System.out.print(" ");
+        }
+        if (count < 9) {
+            System.out.print(" ");
+        }
+        System.out.print(++count + " ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(arrChar[i]);
+        }
+        System.out.print("   ");
+        System.out.flush();
+        if (count % 6 == 0) {
+            System.out.println("");
+        }
+    }
+
+}
