5e1e29677d9db74c88840b92b31062f401080818
Karginskiy <hei@spark-mail.ru>
2016-11-17 15:06:42 +0300

    Entity for LinkedList

---
+++ src/main/java/lafore/linked/Link.java
@@ -0,0 +1,18 @@
+package lafore.linked;
+
+public class Link {
+
+    public int iData;
+    public double dData;
+    public Link next;
+
+    public Link(int id, double dd) {
+        iData = id;
+        dData = dd;
+    }
+
+    public void displayLink() {
+        System.out.print("{" + iData + ", " + dData + "} ");
+    }
+
+}
